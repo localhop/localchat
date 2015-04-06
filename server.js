c95e62f38ea6473eac5c6959fe4a7b264ef6d0d9
Kendal Harland <kharland2011@gmail.com>
2015-04-05 22:52:51 -0500

    basic skeleton chat server working

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
 /** 
- * Local Hop - Backend server "Caboose"
- * 
- * The backend server for Local Hop
+ * Local Hop Chat server
  */
 
 "use strict";
 
 var redisPort = '6379', 
-    redisHost = '127.0.0.1';
+    redisHost = '127.0.0.1',
+    SYSTEM_USER_ID = '0';
 
-var express = require('express.io'),
-    redis = require("redis"),
+var express    = require('express'),
+    app        = express(),
+    redis      = require("redis"),
     bodyParser = require('body-parser'),
-    _ = require('underscore'),
-    colors = require('colors'),
-    app = express(),
+    _          = require('underscore'),
+    colors     = require('colors'),
+    chatRooms  = {},
     client = redis.createClient(redisPort, redisHost);
 
-app.http().io();
 app.set('port', 3001);
 app.use(bodyParser.urlencoded({ extended: false }));
+
+///////////////////////////////////////////////////////////////////////////////
+/// User Interface configuration
+///////////////////////////////////////////////////////////////////////////////
+
+function ChatWelcomeMessage(eventId) {
+  return "Welcome to the "+eventId+" chat room!";
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 /// Redis Client Setup
@@ -105,13 +112,80 @@ function handleRedisQueryErr(err, res) {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+/// Message Marshalling
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 /// Server paths
 ///////////////////////////////////////////////////////////////////////////////
 
-// app.post('', function(req, res) {
 
+/** Called when a new chat room is created for an event */
+app.post("/event/chatroom", function(req, res) {
+  var theEvent = req.body.eventId,
+      theEventClient;
+
+  chatRooms[theEvent] = redis.createClient(redisPort, redisHost);
+  theEventClient = chatRooms[theEvent];
+
+  client.rpush("event:"+theEvent+":messages", ChatWelcomeMessage(theEvent));
+  client.sadd("event:"+theEvent+":users", SYSTEM_USER_ID);
+
+  debug("Chat room created for event "+theEvent);
+
+  theEventClient.on("subscribe", function(channel, count) {
+    debug("Chat room: "+channel+" listening for messages...");
+  });
+
+  theEventClient.on("message", function(channel, message) {
+    debug("Chat room: "+channel+" received message");
+    client.rpush(channel, message);
+  });
+
+  theEventClient.subscribe("event:"+theEvent+":channel");
+  res.send("Chat room for "+theEvent+" created.");
+});
+
+
+/** Called when a client posts a message to a chat room */
+app.post("/message", function(req, res) {
+    var theEvent = req.body.eventId,
+        user = req.body.userId,
+        message = req.body.message;
+    client.publish("event:"+theEvent+":channel", message);
+    res.send('1');
+});
+
+
+/** Called when a client requests the messages from a chat room */
+app.get("/messages/:eventId/:userId", function(req, res) {
+  var theEvent = req.params.eventId,
+      user = req.params.userId;
+  debug("got message request");
+  client.lrange("event:"+theEvent+":messages", "0", "-1", 
+    function (err, reply) {
+      if (err != null)
+        error(err);
+      res.send(reply);
+  }); 
+});
+
+
+// /** Called when a user joins the chat room for the event */
+// app.post("/event/chatuser/:eventId/:userId", function(req, res) {
+//   var theEvent = req.body.eventId,
+//       theUser = req.body.userId;
+
+//   client.sadd("event:"+theEvent+":users", theUser);
 // });
 
+
+// /** Called when a client leaves the chat room for an event */
+// app.delete("/event/chatuser/:eventId/:userId", function(req, res) {
+//   var theEvent = req.body.eventId,
+//       theUser = req.body.userId;
+
+//   client.srem("event:"+theEvent+":users", theUser);
+// });
 
 app.listen(app.get('port'));
 console.log("Server listening on port "+app.get('port')+"...");
--- tests/chat.js
+++ tests/chat.js
@@ -1,6 +1,5 @@
-var io = require('socket.io')(),
-	socket = io.connect("http://localhost:3001"),
-	request = require('request');
+var request = require('request');
+var host = "http://localhost:3001";
 
 function callback(error, response, body) {
   if (!error  && response.statusCode == 200) {
@@ -10,12 +9,29 @@ function callback(error, response, body) {
   }
 }
 
-/** Send a message */
-var message = {
-	room: "1",
-	message: "Hello world!",
-};
+var kendal = "Kendal",
+    adam = "Adam",
+    eventId = 1,
+    message = "Hello chat room!";
 
-socket.emit("message:write", message);
+// Create a chat room
+// console.log("creating chatroom "+eventId);
+// request.post(host+"/event/chatroom", callback).form({
+//   eventId: eventId
+// });
+
+// Post a message to the chat room
+// console.log("posting message: "+message);
+// request.post(host+"/message", callback).form({
+//   eventId: eventId,
+//   userId: kendal,
+//   message: message
+// });
+
+// // // Get all messages from the chat room
+console.log("requesting all messages...");
+request.get(host+"/messages/"+eventId+"/"+kendal, callback);
+
+//request.post().form();
 //request.get(host+"/message", callback).form(message);
 
