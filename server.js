5b4d33ace2617baedf0a46126e25f0aa54307e9c
Kendal Harland <kharland2011@gmail.com>
2015-04-05 20:42:41 -0500

    added redis event handlers

--- server.js
+++ server.js
@@ -6,25 +6,69 @@
 
 "use strict";
 
-// config 
-var dbPort = 6379,
-    dbHost = '127.0.0.1';
+var redisPort = '6379', 
+    redisHost = '127.0.0.1';
 
 var express = require('express.io'),
-    app = express(),
-    redis = require("node-redis"),
-    client = redis.createClient(dbPort, dbHost /*no auth*/),
+    redis = require("redis"),
     bodyParser = require('body-parser'),
     _ = require('underscore'),
-    colors = require('colors');
+    colors = require('colors'),
+    app = express(),
+    client = redis.createClient(),
 
 app.http().io();
 app.set('port', 3001);
 app.use(bodyParser.urlencoded({ extended: false }));
 
 ///////////////////////////////////////////////////////////////////////////////
+/// Redis Client Setup
+///////////////////////////////////////////////////////////////////////////////
+
+client.on("error", handleRedisError);
+client.on("ready", handleRedisReady);
+client.on("end", handleRedisEnd);
+client.on("drain", handleRedisDrain);
+
+///////////////////////////////////////////////////////////////////////////////
 /// Utility functions
 ///////////////////////////////////////////////////////////////////////////////
+
+function handleRedisError(err) { error(err); }
+
+/** 
+ * Emitted when connection to server established and both are ready to receive 
+ * commands.
+ */
+function handleRedisReady() {
+  debug("Redis: client connected to server.");
+  debug("Redis: server ready for commands.");
+}
+
+/** 
+ * Emitted when an established Redis server connection has closed.
+ */
+function handleRedisEnd() {
+  debug("Redis: connection to server lost.");
+}
+
+/** 
+ * Emitted when the TCP connection to the Redis server has been buffering, but
+ * is now writable. This event can be used to stream commands in to Redis and 
+ * adapt to backpressure. Right now, you need to check 
+ * client.command_queue.length to decide when to reduce your send rate. Then 
+ * you can resume sending when you get drain.
+ */
+function handleRedisDrain() {
+  debug("Redis: TCP connection writable.");
+}
+
+/** 
+ * Emitted when there are no outstanding commands that are awaiting a response.
+ */
+function handleRedisIdle() {
+  debug("Redis: idle");
+}
 
 function error(e) { 
   console.error('error:'.bold.red, e); 
@@ -64,22 +108,10 @@ function handleRedisQueryErr(err, res) {
 /// Server paths
 ///////////////////////////////////////////////////////////////////////////////
 
-app.io.route('event', function(req) {
-  debug(req.data);
-  req.io.join(req.data);
-  req.io.broadcast("announce", {
-    message: "joined room:" + req.data
-  });
-});
+// app.post('', function(req, res) {
 
-app.io.route('message', {
-  write: function(req) {
-    debug("message:write" + req.data);
-  },
-  read: function(req) {
-    debug("message:read" + req.data);
-  }
-});
+// });
+
 
 app.listen(app.get('port'));
 console.log("Server listening on port "+app.get('port')+"...");
