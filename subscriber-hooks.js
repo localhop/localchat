29c54b6d5b99872268e4c86cbbd5b6aa4b24cb13
Kendal Harland <kharland2011@gmail.com>
2015-04-09 12:01:07 -0500

    added log_message hook

--- subscriber-hooks.js
+++ subscriber-hooks.js
@@ -1,8 +1,10 @@
 var exports = {
-
-  store_message: function(channel, message) {
-    debug("Chat room: "+channel+" received message");
+  store_message: function(channel, message, ctx) {
+    ctx.debug("Chat room: "+channel+" received message");
     client.rpush(channel, message);
   }),
-  
+
+  log_message: function(channel, message, ctx) {
+    ctx.debug(channel + " received: " + message);
+  }
 };
